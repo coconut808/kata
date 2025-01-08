5b8860a9df28dd8b17386fc8d3330c2c5c70242b
coconut808 <eddaddy@gmail.com>
2025-01-08 09:50:41 -0800

    Updated script to just scan top ports since PortScanner()

--- nscan_one.py
+++ nscan_one.py
@@ -5,24 +5,20 @@ import optparse
 
 nmap = nmap3.Nmap()
 
-def nmapScan(tgtHost, tgtPort):
-    nmScan = nmap.PortScanner()
-    nmScan.scan(tgtHost,tgtPort)
-    state = nmScan[tgtHost]['tcp'][int(tgtPort)]['state']
-    print("[*]" + tgtHost + "tcp/" + tgtPort + " " + state)
+def nmapScan(tgtHost):
+    results = nmap.scan_top_ports(tgtHost)
+    print(results)
 
 def main():
-    parser = optparse.OptionParser('usage%prog -H <target host> -p <target port>')
+    parser = optparse.OptionParser('usage%prog -H <target host> >')
     parser.add_option('-H',dest='tgtHost',type='string',help='specify target host')
-    parser.add_option('-p',dest='tgtPort',type='string',help='specify target port[s] separated by comma')
     (options,args) = parser.parse_args()
     tgtHost = options.tgtHost
-    tgtPorts = str(options.tgtPort).split(',')
-    if(tgtHost == None)|(tgtPorts[0] == None):
+    if(tgtHost == None):
         print(parser.usage)
         exit(0)
-    for tgtPort in tgtPorts:
-        nmapScan(tgtHost, tgtPort)
+    else:
+        nmapScan(tgtHost)
 
 if __name__ == '__main__':
     main()
